15727b72a7568879aba53b0126866521fbd92dad
Brandon Allred <b32alls@gmail.com>
2018-01-15 00:08:43 -0700

    adding the add component

--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
 
 export default class Sum extends Component {
+    constructor () {
+        super ()
+        this.state = {
+            num1: 0,
+            num2: 0,
+            sum: null
+
+        }
+    }
+    updateNum1 (val) {
+        this.setState({num1:parseInt(val,10)})
+    }
+    updateNum2 (val) {
+        this.setState({num2:parseInt(val,10)})
+    }
+
+    add (num1,num2) {
+        this.setState({sum: num1+num2})
+    }
+
     render () {
         return (
-            <p> Sum Component </p>
+            <div className="puzzleBox sumPB">
+                <h4>Sum</h4>
+                <input className="inputLine" onChange={ (e) => {this.updateNum1(e.target.value)}}></input>
+                <input className="inputLine" onChange={ (e) => {this.updateNum2(e.target.value)}}></input>
+                <button className="confirmationButton" onClick={ () => this.add(this.state.num1,this.state.num2)}>Check result</button>
+                <span className="resultsBox">Sum: {this.state.sum} </span>
+            </div>
         )
     }
 }
